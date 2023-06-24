219e7e4db9e713da767b86fc46d5473b99d81dd2
Ahmed.net <Ahmed.net@gmail.com>
2023-06-24 11:57:13 +0300

    Adding structure of project,style,logo

---
+++ main.js
@@ -0,0 +1,5 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("search-btn");
+
